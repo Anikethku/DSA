a0f8438e032db961dd58887fe35446ae6ae6cecc
Anikethku <anikethku2005@gmail.com>
2026-08-26 21:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (95.97%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        for (int m = k; m <= s.length(); m++) {
-            string ans = "";
-            for (int i = m; i <= s.length(); i++) {
-                string t = s.substr(i - m, m);
-                if ((ans.empty() || t < ans) && ranges::count(t, '1') == k) {
-                    ans = t;
+       int one=0,i=0,j=0,flag=0,mini=INT_MAX,idx1=-1,idx2=-1;
+       while(i<s.size()){
+          if(s[i]=='1'){
+            one++;
+          }
+          while(one==k){
+                if(mini==i-j+1){
+                  int val1=idx1;
+                  int val2=j;
+                   while(val2<i){
+                     if(s[val2]<s[val1]){
+                        idx1=j;
+                        idx2=i;
+                        break;
+                     }
+                     if(s[val2]>s[val1]) break;
+                     val1++;
+                     val2++;
+                   } 
+                }else if(mini>i-j+1){
+                    mini=i-j+1;
+                    idx1=j;
+                    idx2=i;
                 }
+            
+            flag=1;
+            if(s[j]=='1'){
+                one--;
             }
-            if (!ans.empty()) {
-                return ans;
-            }
-        }
+            j++;
+          }
+          i++;
+       } 
+       if(flag==0){
         return "";
+       }
+       return s.substr(idx1,idx2-idx1+1);
     }
 };
